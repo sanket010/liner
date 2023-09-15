2d2c6b17962c3d2a83ef16accbabeaa8e700134e
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-09-15 16:54:21 +0530

    Add files via upload

---
+++ container1.js
@@ -0,0 +1,34 @@
+
+import mainlogo from './imgs/logoc1.png'
+import icon1 from './imgs/iconc1.png'
+import icon2 from './imgs/iconc2.png'
+import icon3 from './imgs/iconc3.png'
+
+
+
+
+
+export default function Container1()  {
+return(
+    <>
+    
+    <div className='icon'>
+        <img src={icon2} className='icon2' alt="" height={80} width={90}   />
+         <img src={icon1} className='icon1'  alt="" height={80} width={90}/>
+        <img src={icon3} className='icon3' alt="" height={80} width={90}/>
+        <img src={mainlogo} className='logoc' alt="" height={50} width={60} />
+        </div>
+        
+   
+   <div className='container'>
+        
+        <h1>Transform Text to Design</h1><h1>in Minutes, Powered by AI</h1>
+        </div>
+    
+        </>
+        )
+
+    
+
+}
+
