1d4d5cd872dc58b095b8bb922fef729c87efa6ad
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-09-23 13:15:53 +0530

    Add files via upload

---
+++ container2.jsx
@@ -0,0 +1,40 @@
+import  React  from 'react';
+import './container.css';
+
+
+
+
+
+
+
+const Containertwo = () => {
+    return(
+        <div className='bby'>
+
+
+       
+
+        <> <h1>Playground </h1></>
+         <div>
+            <img src='./imgs/genrate.png' alt=''/>
+      <input
+        
+        type="text"
+        placeholder="create a stunning photography agencey landing page"
+         />
+        <button className="btn btn-outline-success" type="submit">Generate</button>
+     
+    </div>
+
+        </div>
+
+
+
+
+
+
+    );
+};
+
+
+export default Containertwo;
