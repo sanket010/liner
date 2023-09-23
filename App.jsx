3147a9fd728bb9a2f8fa497cdb6c8b90e94537a4
Sanket Tembhare <71713746+sanket010@users.noreply.github.com>
2023-09-23 10:42:50 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,14 +4,25 @@ import './App.css';
 import Navbar from './components/Navbar'
 import './components/container.css';
 import CurvedIconLayout from './components/container1';
+import Containertwo from './components/container2';
 
 function App() {
   return ( 
+
+<>
+    <>
+      <Navbar title="liner" /></>
+      <>
+      <CurvedIconLayout title="head" /></>
+      <>
+        <Containertwo title="body"/></>
+        </>
     
-   
-    <><><>
-      <Navbar title="liner" /></><>
-        </></><CurvedIconLayout title="head" /></>
+   );
+}
+
+export default App;
+    
 
     
    
